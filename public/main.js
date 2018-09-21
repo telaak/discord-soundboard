0451ee3f38696cd3b4ae88367e6089bbd559d849
laaksonen.me VPS <teemu@laaksonen.me>
2018-09-21 18:01:52 +0200

    folder deletion

--- public/main.js
+++ public/main.js
@@ -1,40 +1,42 @@
-let socket = io();
+let socket = io()
 let tree = []
 onload = function () {
+  setTimeout(() => {
     fetch('http://laaksonen.me:8080/files')
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (myJson) {
-            tree = myJson
-            buildFileTree()
-            createEmitters()
-        });
+      .then(function (response) {
+        return response.json()
+      })
+      .then(function (myJson) {
+        tree = myJson
+        buildFileTree()
+        createEmitters()
+      })
+  }, 1)
 }
 
-function buildFileTree() {
-    let fileTree = document.getElementById('fileTree')
-    tree.forEach(element => {
-        let div = document.createElement('div')
-        div.setAttribute('id', element.folder)
-        let ul = document.createElement('ul')
-        div.textContent = element.folder
-        element.files.forEach(file => {
-            let li = document.createElement('li')
-            li.addEventListener('click', function (evt) {
-                socket.emit('playFile', element.folder + "/" + file)
-                emitVolume()
-            })
-            li.textContent = file
-            ul.appendChild(li)
-        })
-        div.appendChild(ul)
-        fileTree.appendChild(div)
-    });
+function buildFileTree () {
+  let fileTree = document.getElementById('fileTree')
+  tree.forEach(element => {
+    let div = document.createElement('div')
+    div.setAttribute('id', element.folder)
+    let ul = document.createElement('ul')
+    div.textContent = element.folder
+    element.files.forEach(file => {
+      let li = document.createElement('li')
+      li.addEventListener('click', function (evt) {
+        socket.emit('playFile', element.folder + '/' + file)
+        emitVolume()
+      })
+      li.textContent = file
+      ul.appendChild(li)
+    })
+    div.appendChild(ul)
+    fileTree.appendChild(div)
+  })
 }
 
-function createEmitters() {
-    /* let links = document.querySelectorAll('a')
+function createEmitters () {
+  /* let links = document.querySelectorAll('a')
      for (let i = 0; i < links.length; i++) {
          links[i].addEventListener('click', function (evt) {
              socket.emit('playFile', links[i].id)
@@ -42,52 +44,52 @@ function createEmitters() {
          });
      } */
 
-    let slider = document.getElementById('slider')
-    slider.addEventListener('input', function (evt) {
-        socket.emit('volume', this.value)
-    })
+  let slider = document.getElementById('slider')
+  slider.addEventListener('input', function (evt) {
+    socket.emit('volume', this.value)
+  })
 }
 
-function emitVolume() {
-    let slider = document.getElementById('slider')
-    socket.emit('volume', slider.value)
+function emitVolume () {
+  let slider = document.getElementById('slider')
+  socket.emit('volume', slider.value)
 }
 
-function emitTube() {
-    socket.emit('playUrl', document.getElementById("youtube").value)
+function emitTube () {
+  socket.emit('playUrl', document.getElementById('youtube').value)
 }
-function stopPlaying() {
-    socket.emit('stopPlaying')
+function stopPlaying () {
+  socket.emit('stopPlaying')
 }
-function pausePlaying() {
-    socket.emit('pausePlaying')
+function pausePlaying () {
+  socket.emit('pausePlaying')
 }
-function resumePlaying() {
-    socket.emit('resumePlaying')
-}
-
-function logOut() {
-    socket.emit('logOut')
+function resumePlaying () {
+  socket.emit('resumePlaying')
 }
 
-function replay() {
-    socket.emit('replay')
-    emitVolume()
+function logOut () {
+  socket.emit('logOut')
+}
+
+function replay () {
+  socket.emit('replay')
+  emitVolume()
 }
 
 socket.on('volume', function (value) {
-    let slider = document.getElementById('slider')
-    slider.value = value
+  let slider = document.getElementById('slider')
+  slider.value = value
 })
 
-socket.on('nowPlaying', function (name, url = "") {
-    let nowPlaying = document.getElementById('nowPlaying')
-    if (url !== "") {
-        let html = "<a href='" + url + "'>" + name + "</a>"
-        nowPlaying.innerHTML = html
-    } else {
-        nowPlaying.textContent = name
-    }
+socket.on('nowPlaying', function (name, url = '') {
+  let nowPlaying = document.getElementById('nowPlaying')
+  if (url !== '') {
+    let html = "<a href='" + url + "'>" + name + '</a>'
+    nowPlaying.innerHTML = html
+  } else {
+    nowPlaying.textContent = name
+  }
 })
 
 const parseTreeFromFileName = fileName => fileName.split('/')
@@ -98,42 +100,50 @@ const getFolderIndex = folderName => tree.findIndex(object => object.folder > fo
 const getUnorderedList = folderName => document.getElementById(folderName).getElementsByTagName('ul')[0]
 
 socket.on('newFile', function (filePath) {
-    let treeArray = parseTreeFromFileName(filePath)
-    let folderName = treeArray[0]
-    let fileName = treeArray[1]
-    let folder = getFolder(folderName)
-    let index = getFileIndex(folder, fileName)
-    folder.splice(index, 0, fileName)
-    let ul = getUnorderedList(folderName)
-    let li = document.createElement('li')
-    li.addEventListener('click', function (evt) {
-        socket.emit('playFile', filePath)
-        emitVolume()
-    })
-    li.textContent = fileName
-    ul.insertBefore(li, ul.childNodes[index])
+  let treeArray = parseTreeFromFileName(filePath)
+  let folderName = treeArray[0]
+  let fileName = treeArray[1]
+  let folder = getFolder(folderName)
+  let index = getFileIndex(folder, fileName)
+  folder.splice(index, 0, fileName)
+  let ul = getUnorderedList(folderName)
+  let li = document.createElement('li')
+  li.addEventListener('click', function (evt) {
+    socket.emit('playFile', filePath)
+    emitVolume()
+  })
+  li.textContent = fileName
+  ul.insertBefore(li, ul.childNodes[index])
 })
 
 socket.on('fileDeleted', function (filePath) {
-    let treeArray = parseTreeFromFileName(filePath)
-    let folderName = treeArray[0]
-    let fileName = treeArray[1]
-    let folder = getFolder(folderName)
-    let index = getFileIndex(folder, fileName)
-    folder.splice(index - 1, 1)
-    let ul = getUnorderedList(folderName)
-    ul.removeChild(ul.childNodes[index - 1])
+  let treeArray = parseTreeFromFileName(filePath)
+  let folderName = treeArray[0]
+  let fileName = treeArray[1]
+  let folder = getFolder(folderName)
+  let index = getFileIndex(folder, fileName)
+  folder.splice(index - 1, 1)
+  let ul = getUnorderedList(folderName)
+  ul.removeChild(ul.childNodes[index - 1])
 })
 
 socket.on('newFolder', function (folderObject) {
-    let fileTree = document.getElementById('fileTree')
-    let folderName = folderObject.folder
-    let index = getFolderIndex(folderName)
-    tree.splice(index, 0, folderObject)
-    let div = document.createElement('div')
-    div.setAttribute('id', folderObject.folder)
-    div.textContent = folderObject.folder
-    let ul = document.createElement('ul')
-    div.appendChild(ul)
-    fileTree.insertBefore(div, fileTree.childNodes[index])
+  let fileTree = document.getElementById('fileTree')
+  let folderName = folderObject.folder
+  let index = getFolderIndex(folderName)
+  tree.splice(index, 0, folderObject)
+  let div = document.createElement('div')
+  div.setAttribute('id', folderObject.folder)
+  div.textContent = folderObject.folder
+  let ul = document.createElement('ul')
+  div.appendChild(ul)
+  fileTree.insertBefore(div, fileTree.childNodes[index])
 })
+
+socket.on('folderDeleted', function (folderName) {
+  let folderIndex = getFolderIndex(folderName)
+  tree.splice(folderIndex - 1, 1)
+  let div = document.getElementById(folderName)
+  div.parentNode.removeChild(div)
+})
+
--- soundboard.js
+++ soundboard.js
@@ -1,159 +1,166 @@
 const express = require('express')
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const fs = require('fs');
+const app = require('express')()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+const fs = require('fs')
 const cors = require('cors')
 const Bot = require('./bot.js')
 const hound = require('hound')
-const fetch = require('node-fetch');
-
-
+const fetch = require('node-fetch')
 app.use(express.static('public'))
 app.use(cors())
 
 class SoundBoard {
-    constructor(path, botToken, channelName, googleApiKey) {
-        this.bot = new Bot(botToken, path, channelName)
-        this.watcher = hound.watch(path)
-        this.path = path
-        this.tree = []
-        this.googleApiKey = googleApiKey
-        this.listen()
-        this.express()
-        this.socketIO()
-        this.getFiles()
-        this.watchFileChanges()
-        this.getFolder = folderName => this.tree.find(object => object.folder === folderName).files
-        this.getFolderIndex = folderName => this.tree.findIndex(object => object.folder > folderName)
-        this.getIndex = (folder, fileName) => folder.findIndex(letter => letter > fileName)
-        this.getFilePathEnd = path => path.split("//")[1]
-    }
+  constructor (path, botToken, channelName, googleApiKey) {
+    this.bot = new Bot(botToken, path, channelName)
+    this.watcher = hound.watch(path)
+    this.path = path
+    this.tree = []
+    this.googleApiKey = googleApiKey
+    this.listen()
+    this.express()
+    this.socketIO()
+    this.getFiles()
+    this.watchFileChanges()
+    this.getFolder = folderName => this.tree.find(object => object.folder === folderName).files
+    this.getFolderIndex = folderName => this.tree.findIndex(object => object.folder > folderName)
+    this.getIndex = (folder, fileName) => folder.findIndex(letter => letter > fileName)
+    this.getFilePathEnd = path => path.split('//')[1]
+  }
 
-    watchFileChanges() {
-        this.watcher.on('create', (file, stats) => {
-            setTimeout(() => {
-                if (fs.lstatSync(file).isDirectory()) {
-                    let object = {}
-                    object.folder = this.getFilePathEnd(file).split("/")[0]
-                    object.files = []
-                    let index = this.getFolderIndex(object.folder)
-                    this.tree.splice(index, 0, object)
-                    io.emit('newFolder', object)
-                } else {
-                    let filePath = this.getFilePathEnd(file)
-                    let treeArray = filePath.split("/")
-                    let folderName = treeArray[0]
-                    let fileName = treeArray[1]
-                    let folder = this.getFolder(folderName)
-                    let index = this.getIndex(folder, fileName)
-                    folder.splice(index, 0, fileName)
-                    io.emit('newFile', filePath)
-                }
-            }, 10);
-        })
-        this.watcher.on('delete', file => {
-            let filePath = this.getFilePathEnd(file)
-            let treeArray = filePath.split("/")
-            let folderName = treeArray[0]
-            let fileName = treeArray[1]
-            let folder = this.getFolder(folderName)
-            let index = this.getIndex(folder, fileName)
-            folder.splice(index, 1)
-            io.emit('fileDeleted', filePath)
-        })
-    }
+  watchFileChanges () {
+    this.watcher.on('create', (file, stats) => {
+      setTimeout(() => {
+        if (fs.lstatSync(file).isDirectory()) {
+          let object = {}
+          object.folder = this.getFilePathEnd(file).split('/')[0]
+          object.files = []
+          let index = this.getFolderIndex(object.folder)
+          this.tree.splice(index, 0, object)
+          io.emit('newFolder', object)
+        } else {
+          let filePath = this.getFilePathEnd(file)
+          let treeArray = filePath.split('/')
+          let folderName = treeArray[0]
+          let fileName = treeArray[1]
+          let folder = this.getFolder(folderName)
+          let index = this.getIndex(folder, fileName)
+          folder.splice(index, 0, fileName)
+          io.emit('newFile', filePath)
+        }
+      }, 10)
+    })
+    this.watcher.on('delete', file => {
+      if (this.getFilePathEnd(file).includes('/')) {
+        let filePath = this.getFilePathEnd(file)
+        let treeArray = filePath.split('/')
+        let folderName = treeArray[0]
+        let fileName = treeArray[1]
+        let folder = this.getFolder(folderName)
+        let index = this.getIndex(folder, fileName)
+        folder.splice(index, 1)
+        io.emit('fileDeleted', filePath)
+      } else {
+        let folderName = this.getFilePathEnd(file)
+        let folderIndex = this.getFolderIndex(folderName)
+        this.tree.splice(folderIndex, 1)
+        io.emit('folderDeleted', folderName)
+      }
+    })
+  }
 
-    sortFiles() {
-        this.tree.forEach(object => {
-            object.files.sort()
-        })
-    }
+  sortFiles () {
+    this.tree.forEach(object => {
+      object.files.sort()
+    })
+  }
 
-    listen() {
-        http.listen(8080, () => {
-            console.log('listening on *:8080');
-        });
-    }
+  listen () {
+    http.listen(8080, () => {
+      console.log('listening on *:8080')
+    })
+  }
 
-    socketIO() {
-        io.on('connection', socket => {
-            socket.on('playFile', fileName => {
-                if (!this.bot.isPlaying) {
-                    this.bot.play(fileName)
-                    io.emit('nowPlaying', fileName)
-                }
-            });
-            socket.on('playUrl', url => {
-                if (!this.bot.isPlaying) {
-                    this.bot.play(url)
-                    this.getYoutubeInfo(url)
-                }
-            })
-            socket.on('stopPlaying', () => {
-                this.bot.stopPlaying()
-            })
-            socket.on('pausePlaying', () => {
-                this.bot.pausePlaying()
-            })
-            socket.on('resumePlaying', () => {
-                this.bot.resumePlaying()
-            })
-            socket.on('volume', value => {
-                if (value <= 2.5 && this.bot.isPlaying) {
-                    this.bot.setVolume(value)
-                    socket.broadcast.emit('volume', value)
-                }
-            })
-            socket.on('logOut', () => {
-                this.bot.logOut()
-            })
-            socket.on('replay', () => {
-                this.bot.replay()
-            })
-        });
-    }
+  socketIO () {
+    io.on('connection', socket => {
+      socket.on('playFile', fileName => {
+        if (!this.bot.isPlaying) {
+          this.bot.play(fileName)
+          io.emit('nowPlaying', fileName)
+        }
+      })
+      socket.on('playUrl', url => {
+        if (!this.bot.isPlaying) {
+          this.bot.play(url)
+          this.getYoutubeInfo(url)
+        }
+      })
+      socket.on('stopPlaying', () => {
+        this.bot.stopPlaying()
+      })
+      socket.on('pausePlaying', () => {
+        this.bot.pausePlaying()
+      })
+      socket.on('resumePlaying', () => {
+        this.bot.resumePlaying()
+      })
+      socket.on('volume', value => {
+        if (value <= 2.5 && this.bot.isPlaying) {
+          this.bot.setVolume(value)
+          socket.broadcast.emit('volume', value)
+        }
+      })
+      socket.on('logOut', () => {
+        this.bot.logOut()
+      })
+      socket.on('replay', () => {
+        this.bot.replay()
+      })
+    })
+  }
 
-    getYoutubeInfo(url, key = this.googleApiKey) {
-        let id = url.split('=')[1]
-        fetch('https://www.googleapis.com/youtube/v3/videos?part=snippet&id=' + id + '&key=' + key)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (myJson) {
-                io.emit('nowPlaying', myJson.items[0].snippet.title, url)
-            });
-    }
+  getYoutubeInfo (url, key = this.googleApiKey) {
+    let id = url.split('=')[1]
+    fetch('https://www.googleapis.com/youtube/v3/videos?part=snippet&id=' + id + '&key=' + key)
+      .then(function (response) {
+        return response.json()
+      })
+      .then(function (myJson) {
+        io.emit('nowPlaying', myJson.items[0].snippet.title, url)
+      })
+  }
 
-    getFiles() {
-        fs.readdirSync(this.path).forEach(file => {
-            if (file !== '.dropbox' & file !== 'desktop.ini')
-                if (fs.lstatSync(this.path + file).isDirectory()) {
-                    let json = {}
-                    let files = []
-                    json.folder = file
-                    fs.readdirSync(this.path + file).forEach(folderFile => {
-                        files.push(folderFile)
-                    })
-                    json.files = files
-                    this.tree.push(json)
-                } else {
+  getFiles () {
+    fs.readdirSync(this.path).forEach(file => {
+      if (file !== '.dropbox' & file !== 'desktop.ini') {
+        if (fs.lstatSync(this.path + file).isDirectory()) {
+          let json = {}
+          let files = []
+          json.folder = file
+          fs.readdirSync(this.path + file).forEach(folderFile => {
+            files.push(folderFile)
+          })
+          json.files = files
+          this.tree.push(json)
+        } else {
 
-                }
-        })
-    }
+        }
+      }
+    })
+  }
 
-    express() {
-        app.get('/files/', (req, res) => {
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.write(JSON.stringify(this.tree))
-            return res.end()
-        })
+  express () {
+    app.get('/files/', (req, res) => {
+      res.writeHead(200, { 'Content-Type': 'application/json' })
+      res.write(JSON.stringify(this.tree))
+      return res.end()
+    })
 
-        app.get('/', (req, res) => {
-            res.sendFile('index.html', { root: __dirname })
-        })
-    }
+    app.get('/', (req, res) => {
+      res.sendFile('index.html', { root: __dirname })
+    })
+  }
 }
 
 module.exports = SoundBoard
+
